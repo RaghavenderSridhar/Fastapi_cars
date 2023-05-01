1b891f5a286ea072097e1c1b4a6debaa9984a76e
raghavendersridhar <raghav.ajay3@gmail.com>
2023-05-01 23:05:51 +0530

    jinja templates

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI, Query, Path,HTTPException, status, Body
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request
 from fastapi.encoders import jsonable_encoder
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from models import Car
 from database import cars
 from typing import Optional, List, Dict
 from pydantic import BaseModel, Field
+from starlette.responses import HTMLResponse
+from starlette.status import HTTP_400_BAD_REQUEST
 
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"hello world" : "base root"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/cars", response_model= List[Dict[str, Car]])
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    # return {"hello world": "base root"}
+    return templates.TemplateResponse("home.html", {'request': request})
+
+
+@app.get("/cars", response_model=List[Dict[str, Car]])
 def get_cars(number: Optional[str] = Query("10", max_length=3)):
     response = []
     for id, car in list(cars.items())[:int(number)]:
@@ -21,44 +31,46 @@ def get_cars(number: Optional[str] = Query("10", max_length=3)):
         response.append(to_add)
     return response
 
-@app.get("/cars/{id}", response_model= Car)
-def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+
+@app.get("/cars/{id}", response_model=Car)
+def get_car_by_id(id: int = Path(..., ge=0, lt=1000)):
     car = cars.get(id)
     if not car:
-        raise HTTPException( status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return car
 
-@app.post("/cars", status_code = status.HTTP_201_CREATED)
-def add_cars(body_cars : List[Car], min_id: Optional[int] = Body(0)):
+
+@app.post("/cars", status_code=status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
     if len(body_cars) < 1:
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
+                            detail="No cars to add.")
     min_id = len(cars.values()) + min_id
     for car in body_cars:
         while cars.get(min_id):
             min_id += 1
         cars[min_id] = car
         min_id += 1
-        
-@app.put("/cars/{id}", response_model=Dict[str, Car] )    
+
+
+@app.put("/cars/{id}", response_model=Dict[str, Car])
 def update_car(id: int, car: Car = Body(...)):
     stored = cars.get(id)
     if not stored:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="could not find the car")    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="could not find the car")
     stored = Car(**stored)
     new = car.dict(exclude_unset=True)
     new = stored.copy(update=new)
     car[id] = jsonable_encoder(new)
     response = {}
     response[id] = cars[id]
-    return response 
+    return response
+
 
 @app.get("/delete/{id}")
 def delete_car(id: int):
     if not cars.get(id):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="could not find the car")    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="could not find the car")
     del cars[id]
-
-    
-    
-
-
